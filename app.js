34d264898753eb6c70fc702a01e07950ad427703
Aimi Fadhil <59568628+ColonelBucket8@users.noreply.github.com>
2022-01-22 08:10:51 +0800

    added joke and random number routes

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ app.get("/", function (req, res) {
   res.send("HELLO FROM OUR WEB APP!");
 });
 
+app.get("/joke", function (req, res) {
+  let joke = "What do you call a dog that does magic tricks? A labracadabrador";
+  res.send(joke);
+});
+
+app.get("/random_num", function (req, res) {
+  let rand = Math.floor(Math.random() * 10) + 1;
+  res.send("Your lucky number is " + rand);
+});
+
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
